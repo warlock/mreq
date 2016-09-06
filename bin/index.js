77482dbc1d14733f17979b4eb1130cf8e11b54f7
Josep Subils <jsubils@anpro21.cm>
2016-09-06 13:05:46 +0200

    update: es6 strings

--- bin/index.js
+++ bin/index.js
@@ -7,9 +7,12 @@ const deal = require('../lib/dealer.js')
 var i = 0
 var stats_timeout = 3
 var default_cad = 10000
-function stats(seconds) {
+function stats (url, parallel, seconds) {
+  console.log(`URL: ${url}`)
+  console.log(`PARALLEL CADENCY: ${parallel}`)
+  console.log(`STATS TIMEOUT SECONDS: ${seconds}`)
   setInterval(() => {
-    console.log(new Date() + " - REQUEST COUNT: " + i)
+    console.log(`${new Date()} - REQUEST COUNT: ${i}`)
   }, seconds * 1000)
 }
 
@@ -17,35 +20,26 @@ arg.alone(res => {
   if (!sb.empty(sb.get(res, '0')) && sb.isString(sb.get(res, '0')) && sb.get(res, '0') !== '-h' ) {
     if (!sb.empty(sb.get(res, '1')) && sb.get(res, '1') === '-c') {
       if (sb.isInteger(parseInt(sb.get(res, '2')))) {
-        console.log('URL: ' + sb.get(res, '0'))
-        console.log('CADENCY PARALLEL REQUESTS: ' + sb.get(res, '2'))
-        console.log('STATS TIMEOUT SECONDS: ' + stats_timeout)
-        stats(stats_timeout)
-        deal(parseInt(sb.get(res, '2')), function (done) {
-          req.get(sb.get(res, '0'), function () {
+        stats(sb.get(res, '0'), sb.get(res, '2'), stats_timeout)
+        deal(parseInt(sb.get(res, '2')), (done) => {
+          req.get(sb.get(res, '0'), () => {
             i++
             done()
           })
         })
       } else {
-        console.log('-c needs a CADENCY parallel requests')
-        console.log('')
+        console.log(`-c needs a CADENCY parallel requests\n`)
         help(default_cad, stats_timeout)
       }
     } else {
-      console.log('URL: ' + sb.get(res, '0'))
-      console.log('DEFAULT CADENCY PARALLEL REQUESTS: ' + 1)
-      console.log('STATS TIMEOUT SECONDS: ' + stats_timeout)
-      stats(stats_timeout)
-      deal(default_cad, function (done) {
-        req.get(sb.get(res, '0'), function () {
+      stats(sb.get(res, '0'), default_cad, stats_timeout)
+      deal(default_cad, (done) => {
+        req.get(sb.get(res, '0'), () => {
           i++
           console.log(i)
           done()
         })
       })
     }
-  } else {
-    help(default_cad, stats_timeout)
-  }
+  } else help(default_cad, stats_timeout)
 })
