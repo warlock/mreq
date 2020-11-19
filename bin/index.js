bf498cf3028d3d7e413f2ded834705da3b5c785f
Josep Subils <josep.subils@dxc.com>
2020-11-19 17:49:17 +0100

    working in new version

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const arg = require('argcon')
 const snc = require('snc')
-const req = require('../lib/req')
+const got = require('got')
 const help = require('../lib/help')
 var stats_timeout = 3
 var default_cad = 10000
@@ -9,11 +9,12 @@ var counter = 0
 
 const testurl = url => {
   const regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-/]))?/
-  if(!regex.test(url)) return false
+  if (!regex.test(url)) return false
   else return true
 }
 
-arg.on('-c', res => { // CADENCYA
+arg.on('-c', res => {
+  // CADENCYA
   default_cad = res
 })
 
@@ -22,19 +23,35 @@ arg.on('-t', res => {
 })
 
 arg.on(res => {
-  if (res.length < 3) help(default_cad, stats_timeout)
-  else {
-    if (res[2] !== undefined && testurl(res[2])) {
-      console.log(`URL: ${res[2]}\nPARALLEL CADENCY: ${default_cad}\nSTATS TIMEOUT SECONDS: ${stats_timeout}`)
-      setInterval(() => {
-        console.log(`${new Date()} - PARALLEL CADENCY ${default_cad} - REQUEST COUNT: ${counter}`)
-      }, stats_timeout * 1000)
-      snc.fp(default_cad, (count, done) => {
-        counter = count
-        req(res[2])
-          .then(done)
-          .catch(done)
-      })
-    }
+  if (res.length < 3) return help(default_cad, stats_timeout)
+
+  if (!res[2] && !testurl(res[2])) {
+    return help(default_cad, stats_timeout)
   }
+
+  console.log(res)
+
+  process.exit(0)
+
+  console.log(
+    `URL: ${res[2]}\nPARALLEL CADENCY: ${default_cad}\nSTATS TIMEOUT SECONDS: ${stats_timeout}`
+  )
+
+  setInterval(() => {
+    console.log(`${new Date()} - PARALLEL CADENCY ${default_cad} - REQUEST COUNT: ${counter}`)
+  }, stats_timeout * 1000)
+
+  snc.fp(default_cad, (count, done) => {
+    counter = count
+    console.log('iteracio...')
+    got(res[2], { timeout })
+      .then(data => {
+        console.log(data)
+        done()
+      })
+      .catch(error => {
+        console.log(error)
+        done()
+      })
+  })
 })
